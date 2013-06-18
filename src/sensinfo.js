d2408667ff09efafd03f2002aad4879bef3a685b
hotoo <hotoo.cn@gmail.com>
2013-06-18 11:17:54 +0800

    U: fixed #2, 增加敏感信息上下文

--- src/sensinfo.js
+++ src/sensinfo.js
@@ -28,16 +28,21 @@ define(function(require, exports){
 
   exports.scan = function(html){
     var re_cards = /\b\d{11,19}X?\b/g;
-    var m = html.match(re_cards);
-    if(!m){return;}
-    for(var i=0,card,l=m.length; i<l; i++){
-      card = m[i];
+    var re_blank = /\s{2,}|\r|\n/g;
+    var card, result, context, start, length;
+
+    while(result = re_cards.exec(html)){
+      card = result[0];
+      start = Math.max(result.index - 30, 0);
+      length = card.length + 60;
+      context = html.substr(start, length)
+      context = context.replace(re_blank, "");
       if(mobile.verify(card)){
-        monitor.log("mobile="+privacy(card, "3...4"), "sens");
+        monitor.log("mobile="+privacy(card, "3...4")+"```"+context, "sens");
       }else if(idcard.verify(card)){
-        monitor.log("idcard="+privacy(card, "6...4"), "sens");
+        monitor.log("idcard="+privacy(card, "6...4")+"```"+context, "sens");
       }else if(bankcard.verify(card)){
-        monitor.log("bankcard="+privacy(card, "6...4"), "sens");
+        monitor.log("bankcard="+privacy(card, "6...4")+"```"+context, "sens");
       }
     }
   };
